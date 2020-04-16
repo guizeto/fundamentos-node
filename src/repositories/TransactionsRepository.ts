ffe96d4a65b08a6af9ae735a071696a228bf5ac4
Guizeto <g.luizeto@hotmail.com>
2020-04-15 22:05:43 -0300

    Create transaction with balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (acc: Balance, transaction: Transaction): Balance => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.value;
+        }
+
+        if (transaction.type === 'outcome') {
+          acc.outcome += transaction.value;
+        }
+
+        acc.total = acc.income - acc.outcome;
+        return {
+          income: acc.income,
+          outcome: acc.outcome,
+          total: acc.total,
+        };
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
